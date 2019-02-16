07359720b21dd5a226dcb7581645cffff666fda9
Jing Lin <moderatelyfunctionalprogrammer@gmail.com>
2019-02-16 02:49:40 -0500

    working on textfield

--- Footprint/Footprint/ViewControllers/LoginViewController.swift
+++ Footprint/Footprint/ViewControllers/LoginViewController.swift
@@ -43,8 +43,8 @@ class LoginViewController: UIViewController {
     }
 
     func setupButtons() {
-        let emailField = AutoTextField(placeholder: "Email" textColor: UIColor.black, backgroundColor: UIColor.white)
-        let passwordField = AutoTextField(placeholder: "Password" textColor: UIColor.black, backgroundColor: UIColor.white)
+        let emailField = AutoTextField(placeholder: "Email", textColor: UIColor.black, backgroundColor: UIColor.white)
+        let passwordField = AutoTextField(placeholder: "Password", textColor: UIColor.black, backgroundColor: UIColor.white)
         let loginButton = AutoButton(text: "Log In", titleColor: UIColor.white, backgroundColor: LoginVC.primaryGreen)
         
         self.view.addSubview(loginButton)
@@ -53,6 +53,11 @@ class LoginViewController: UIViewController {
         
         self.view.addConstraints(FLayoutConstraint.paddingPositionConstraints(view: loginButton, sides: [.left, .right, .bottom], padding: 40))
         self.view.addConstraint(FLayoutConstraint.constantConstraint(view: loginButton, attribute: .height, value: 40))
+        self.view.addConstraints(FLayoutConstraint.paddingPositionConstraints(view: emailField, sides: [.left, .right], padding: 40))
+        
+        
+        self.view.addConstraints(FLayoutConstraint.paddingPositionConstraints(view: passwordField, sides: [.left, .right], padding: 40))
+
     }
     
 }
--- Footprint/Footprint/ViewControllers/MapsViewController.swift
+++ Footprint/Footprint/ViewControllers/MapsViewController.swift
@@ -7,16 +7,36 @@
 //
 
 import UIKit
+import GoogleMaps
 
 class MapsViewController: UIViewController {
     
+    override func loadView() {
+        // Create a GMSCameraPosition that tells the map to display the
+        // coordinate -33.86,151.20 at zoom level 6.
+        let camera = GMSCameraPosition.camera(withLatitude: -33.86, longitude: 151.20, zoom: 6.0)
+        let mapView = GMSMapView.map(withFrame: CGRect(x: 0, y: 0, width: 100, height: 100), camera: camera)
+        self.view.addSubview(mapView)
+        
+        // Creates a marker in the center of the map.
+        let marker = GMSMarker()
+        marker.position = CLLocationCoordinate2D(latitude: -33.86, longitude: 151.20)
+        marker.title = "Sydney"
+        marker.snippet = "Australia"
+        marker.map = mapView
+    }
+    
     override func viewDidLoad() {
-        let view = UIView(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
-        view.backgroundColor = UIColor.red
+        // let mapView = UIView(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
+        // view.backgroundColor = UIColor.red
+        
+        
+        
+        // self.view.addSubview(<#T##view: UIView##UIView#>)
         
         self.view.backgroundColor = UIColor.green
         
-        self.view.addSubview(view)
+//        self.view.addSubview(mapView)
     }
     
 }
